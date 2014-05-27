7370d3c56ddda8184555a4ff83400a615d6da573
Jaime Caballero <lamatesoft@gmail.com>
2014-05-27 08:36:12 +0200

    Stable, let's get the new form up and working

---
+++ js/core.js
@@ -0,0 +1,20 @@
+// @codekit-prepend 'partial/modernizr.js'
+
+
+/*
+============================
+***** CIEGO.ES JS Core *****
+============================
+
+(C) 2014 - Proudly handcrafted by Jaime Caballero.
+*/
+
+// @codekit-prepend 'partial/default.js'
+// @codekit-prepend 'partial/router.js'
+// @codekit-prepend 'partial/filters.js'
+
+// @codekit-prepend 'factory/Data.js'
+
+
+// @codekit-prepend 'controller/ContentCtrl.js'
+// @codekit-prepend 'controller/MapCtrl.js'
